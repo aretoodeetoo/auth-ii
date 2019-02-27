dc4d2012627b6720a5531edc6f74b433556cd11c
croy <roy.christie95@gmail.com>
2019-02-27 15:03:12 -0600

    Set up usersRouter endpoint file

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 const express = require('express');
 const helmet = require('helmet');
 
-// const usersRouter = require('');
+const usersRouter = require('./users/usersRouter');
 
 // server commands
 const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// server.use('/api/users', usersRouter);
+server.use('/api/users', usersRouter);
 
 server.get('/', async (req, res) => {
     res.send(`It's working!!!`);
