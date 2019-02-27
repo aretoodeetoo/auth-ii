2f251e158089d67ab5e11da9b71cc3719f514193
croy <roy.christie95@gmail.com>
2019-02-27 15:01:19 -0600

    Set up serverJS file

---
+++ server.js
@@ -0,0 +1,18 @@
+// imports
+const express = require('express');
+const helmet = require('helmet');
+
+// const usersRouter = require('');
+
+// server commands
+const server = express();
+server.use(express.json());
+server.use(helmet());
+
+// server.use('/api/users', usersRouter);
+
+server.get('/', async (req, res) => {
+    res.send(`It's working!!!`);
+})
+
+module.exports = server;
